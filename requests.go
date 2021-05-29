357025d984983a2c1231e123a3f88a029c73baf3
Tsubasa <lixuesonlu@qq.com>
2021-05-29 13:24:58 +0800

    fix bug

--- requests.go
+++ requests.go
@@ -15,7 +15,9 @@ var (
 		"User-Agent":   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.70",
 		"Content-Type": "application/x-www-form-urlencoded",
 	}
-	request = &Requests{}
+	request = &Requests{
+		Client: &http.Client{},
+	}
 )
 
 type Requests struct {
